d06c5e87f7ae9fa33181d258339f9ddb69cb168e
Igor Ezhov <dexter.gm@gmail.com>
2019-04-29 12:28:58 +0300

    Some bootstrap for tests and main class, PHPStan shortcut.

--- src/Censored/Censored.php
+++ src/Censored/Censored.php
@@ -11,4 +11,37 @@ class Censored
     {
         # code
     }
+
+    /**
+     * Check text for vocabulary acceptability.
+     *
+     * @param string $text
+     * @return bool
+     */
+    public function isAcceptable(string $text): bool
+    {
+        return empty($text) ? true : false;
+    }
+
+    /**
+     * Censorship of the text.
+     *
+     * @param string $text
+     * @return string
+     */
+    public function censor(string $text): string
+    {
+        return $text;
+    }
+
+    /**
+     * Get prohibited words count.
+     *
+     * @param string $text
+     * @return int
+     */
+    public function getProhibitedWordsCount(string $text): int
+    {
+        return empty($text) ? 0 : -1;
+    }
 }
--- tests/CensoredTest.php
+++ tests/CensoredTest.php
@@ -7,9 +7,49 @@ use PHPUnit\Framework\TestCase;
 
 final class CensoredTest extends TestCase
 {
-    public function testConstructor()
+    /**
+     * @var \Censored\Censored
+     */
+    private $censor;
+
+    /**
+     * @var string
+     */
+    private $bad = 'I will kill you and fuck your body, dumbshit motherfucker!';
+
+    /**
+     * @var string
+     */
+    private $good = 'Any type may be returned, including arrays and objects.';
+
+    public function __construct($name = null, array $data = [], $dataName = '')
     {
-        $censor = new Censored();
-        $this->assertTrue(true);
+        parent::__construct($name, $data, $dataName);
+
+        $this->censor = new Censored();
+    }
+
+    /**
+     * Check text for vocabulary acceptability.
+     */
+    public function testIsAcceptable()
+    {
+        $this->assertFalse($this->censor->isAcceptable($this->bad));
+    }
+
+    /**
+     * Censorship of the text.
+     */
+    public function testCensor()
+    {
+        $this->assertEquals($this->good, $this->censor->censor($this->good));
+    }
+
+    /**
+     * Counting prohibited words in text.
+     */
+    public function testGetProhibitedWordsCount()
+    {
+        $this->assertEquals(0, $this->censor->getProhibitedWordsCount(''));
     }
 }
